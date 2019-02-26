f14ed73a3ad8253f4e4aa7e5852b6715f8df4c36
Ilya Uglyanitsa <iuglyanitsa@elevatecredit.co.uk>
2019-02-26 19:24:45 +0300

    Initialize

---
+++ TestProject/TestProject.Data/Entities/CarEntity.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace TestProject.Data.Entities
+{
+    public class CarEntity
+    {
+        public long Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public long GarageId { get; set; }
+
+        public virtual GarageEntity Garage { get; set; }
+
+        public long CategoryId { get; set; }
+
+        public virtual CarCategoryEntity Category { get; set; }
+
+        public DateTime DateCreated { get; set; }
+
+        public DateTime DateModified { get; set; }
+    }
+}
---
+++ TestProject/TestProject.Data/Entities/GarageEntity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace TestProject.Data.Entities
+{
+    public class GarageEntity
+    {
+        public long Id { get; set; }
+
+        public string Name { get; set; }
+
+        public virtual AreaEntity Area { get; set; }
+
+        public DateTime DateCreated { get; set; }
+
+        public DateTime DateModified { get; set; }
+    }
+}
